1dfa87d55f95a4278619e5175828b9865e14284b
Sanngts <2508177947@qq.com>
2026-04-15 08:35:02 +0800

    提交编辑的文件

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,19 @@
+function showColor(color) {
+    document.getElementById('colorPreview').style.backgroundColor = color;
+}
 
+document.getElementById("btnPick").addEventListener("click", pickcolor);
+async function pickcolor() 
+{
+    try 
+    {
+        const dropper = new EyeDropper();
+        const result = await dropper.open();
+        const color = result.sRGBHex;
+
+        showColor(color);
+    } catch (error) 
+    {
+        console.error(error);
+    }
+}
